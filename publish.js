be574d48e950a90e4f4e019ec48c7b0161cccdd6
hlqbj1 <soutongshequ@gmail.com>
2025-11-27 22:54:13 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -23,74 +23,61 @@ function getRandomSubdomain() {
     var index = Math.floor(Math.random() * subdomains.length);
     return subdomains[index];
 }
+
 function getRandomString() {
     var result = '';
     var characters = 'abcdefghijklmnopqrstuvwxyz';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < 3; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    for (var i = 0; i < 3; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return result;
 }
-var emails = [
-    'hlqbj2025@gmail.com'
-];
 
-var urls=[];
-       urls.push("..ojhdquyn.xyz"); 
-        urls.push(".nmwcauk.xyz");   
-        urls.push(".iwsndnr.xyz");   
-        urls.push(".eogixfo.xyz");              
-                                                                                                                                                      
+var emails = ['hlqbj2025@gmail.com'];
+
+var urls = [];
+urls.push(".ojhdquyn.xyz");
+urls.push(".nmwcauk.xyz");
+urls.push(".iwsndnr.xyz");
+urls.push(".eogixfo.xyz");
+
+// ★ 必须先定义 JumpPage
+var JumpPage = "https://hlqbj.com";
+
+// ★ 修复成无语法错误 + 自动去重
 var foreverUrls = Array.from(new Set([
-	'https://hlqbj.com',
-	JumpPage,
-	'https://hlqbj.net'
+    'https://hlqbj.com',
+    JumpPage,
+    'https://hlqbj.net'
+]));
 
 var newestUrls = [];
 
-
-for(var i =0;i<urls.length*3;i++){
-    newestUrls.push( 'https://' + getRandomSubdomain() +urls[randomNum(0,urls.length-1)]);
+for (var i = 0; i < urls.length * 3; i++) {
+    newestUrls.push('https://' + getRandomSubdomain() + urls[randomNum(0, urls.length - 1)]);
 }
 
-// var newestUrls = [
-//     'https://' + getRandomSubdomain() + '.vikxddcy.org',    
-//     'https://' + getRandomSubdomain() + '.vikxddcy.org',
-//     'https://' + getRandomSubdomain() + '.vikxddcy.org',    
-//     'https://' + getRandomSubdomain() + '.vildiho.cc',
-// 	'https://' + getRandomSubdomain() + '.vildiho.cc',    
-//     'https://' + getRandomSubdomain() + '.vildiho.cc'
-// ];
 var otherUrls = [
-    'https://hlqbj1.github.io',
-];
-var foreverUrls = [
-	'https://hlqbj.com',
-	JumpPage,
-	'https://hlqbj.net',
-];
-var notices = [
-    '* 我们推荐PC和Andriod手机用户使用Chrome(谷歌)浏览器访问，iPhone用户我们建议您使用手机自带Safari浏览器访问。',
-    //'* 为了防止域名劫持而无法访问，请设置手机和PC的DNS，我们推荐使用8.8.8.8和1.1.1.1。',
-    '* 大陆地区用户我们建议您可以使用VPN或者代理的方式来访问我们的永久地址。',
-	'* 大陆地区用户我们强烈建议您截图收藏当前页面。'
+    'https://hlqbj1.github.io'
 ];
 
-//生成从minNum到maxNum的随机数
-function randomNum(minNum,maxNum){ 
-    switch(arguments.length){ 
-        case 1: 
-            return parseInt(Math.random()*minNum+1,10); 
-        break; 
-        case 2: 
-            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
-        break; 
-            default: 
-                return 0; 
-            break; 
-    } 
-} 
+var notices = [
+    '* 我们推荐PC和Andriod手机用户使用Chrome(谷歌)浏览器访问，iPhone用户建议使用自带Safari浏览器。',
+    '* 大陆地区用户建议使用VPN或代理访问永久地址。',
+    '* 大陆地区用户强烈建议截图保存当前页面。'
+];
+
+// 随机数函数
+function randomNum(minNum, maxNum) {
+    switch (arguments.length) {
+        case 1:
+            return parseInt(Math.random() * minNum + 1, 10);
+        case 2:
+            return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+        default:
+            return 0;
+    }
+}
 
 function createFieldElem(option) {
     var title = option.title;
@@ -107,60 +94,76 @@ function createFieldElem(option) {
     titleElem.setAttribute('class', 'title');
     titleElem.innerHTML = title;
     fieldElem.appendChild(titleElem);
-  
-        var ulElem = document.createElement('ul');
- 
-    
-    var htmlStr = ''
-    for (var i = 0; i < items.length; i++) {
-        if (plainText) {
-            htmlStr = htmlStr + '<li>' + items[i] + '</li>';
-        } else {
-            htmlStr = htmlStr + '<li><a href="' + items[i] + '" target="_blank">' + items[i] + '</a></li>';
+
+    var ulElem = document.createElement('ul');
+    var htmlStr = '';
+
+    if (items && items.length > 0) {
+        for (var i = 0; i < items.length; i++) {
+            if (plainText) {
+                htmlStr += '<li>' + items[i] + '</li>';
+            } else {
+                htmlStr += '<li><a href="' + items[i] + '" target="_blank">' + items[i] + '</a></li>';
+            }
         }
     }
 
-    // For text
     if (text) {
-        htmlStr = htmlStr + '<li class="text">' + text + '</li>';
+        htmlStr += '<li class="text">' + text + '</li>';
     }
 
     ulElem.innerHTML = htmlStr;
     fieldElem.appendChild(ulElem);
+
     return fieldElem;
 }
 
 window.onload = function () {
     var mainElem = document.getElementById('main');
-    // append logo
+
     var logoElem = document.createElement('div');
     logoElem.setAttribute('class', 'brand');
     logoElem.setAttribute('id', 'logo');
-    logoElem.innerHTML = ' <img src="logo.png"  width="180"/>'
+    logoElem.innerHTML = '<img src="logo.png" width="180"/>';
     mainElem.appendChild(logoElem);
 
-    //收藏  ‘<br /> \n <br /> <br /> 按 <font color=red><b>Ctrl+D</b></font> 收藏此页，永不迷路
-    var newFavoritesElem =  createFieldElem({title: '<font color=red><b>Ctrl+D</b></font> 收藏此页，永不迷路', items: ""});
+    var newFavoritesElem = createFieldElem({
+        title: '<font color=red><b>Ctrl+D</b></font> 收藏此页，永不迷路',
+        items: ""
+    });
     mainElem.appendChild(newFavoritesElem);
-    // newest urls
-    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问黑料情报局'});
+
+    var newestFieldElem = createFieldElem({
+        title: '最新地址',
+        items: newestUrls,
+        text: '请使用 https:// 协议访问黑料情报局'
+    });
     mainElem.appendChild(newestFieldElem);
 
-    // other urls
-    var otherFieldElem = createFieldElem({title: '由于风控原因，网址经常被墙<br />务必保存下面所有地址发布页', items: otherUrls});
+    var otherFieldElem = createFieldElem({
+        title: '由于风控原因，网址经常被墙<br/>务必保存下面所有地址发布页',
+        items: otherUrls
+    });
     mainElem.appendChild(otherFieldElem);
 
-    // email
-    var mailFieldElem = createFieldElem({title: '发送邮件获得最新地址', items: emails, plainText: true});
+    var mailFieldElem = createFieldElem({
+        title: '发送邮件获得最新地址',
+        items: emails,
+        plainText: true
+    });
     mainElem.appendChild(mailFieldElem);
 
-    // forever urls
-    var foreverFieldElem = createFieldElem({title: '永久地址', items: foreverUrls});
+    var foreverFieldElem = createFieldElem({
+        title: '永久地址',
+        items: foreverUrls
+    });
     mainElem.appendChild(foreverFieldElem);
 
-    // Description
-    var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
+    var noticeFieldElem = createFieldElem({
+        title: '注意事项',
+        items: notices,
+        plainText: true,
+        classStr: 'desc'
+    });
     mainElem.appendChild(noticeFieldElem);
 }
-
-
