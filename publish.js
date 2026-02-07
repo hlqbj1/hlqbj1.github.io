1aeafdb82d93cf6545c2e42e443badaf68a7c9c0
hlqbj1 <soutongshequ@gmail.com>
2026-02-07 17:08:31 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -36,7 +36,7 @@ function getRandomString() {
 var emails = ['hlqbj2025@gmail.com'];
 
 var urls = [];
-urls.push(".uvaqxebk.xyz");
+urls.push("lepxtepc.cc");
 
 // ★ 必须先定义 JumpPage
 var JumpPage = "https://hlqbj.com";
@@ -164,6 +164,7 @@ window.onload = function () {
     });
     mainElem.appendChild(noticeFieldElem);
 }
+
 
 
 
